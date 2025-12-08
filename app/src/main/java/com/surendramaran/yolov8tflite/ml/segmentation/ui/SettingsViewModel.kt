a2abf6dbabb9a12c91d7094962a22a1353a12ba7
Ashish Umrao <ashishumrao632005@gmail.com>
2025-12-08 17:51:52 +0530

    gif and coin removed for round 1 eval

--- app/src/main/java/com/surendramaran/yolov8tflite/ml/segmentation/ui/SettingsViewModel.kt
+++ app/src/main/java/com/surendramaran/yolov8tflite/ml/segmentation/ui/SettingsViewModel.kt
@@ -6,5 +6,5 @@ class SettingsViewModel : ViewModel() {
     var isSeparateOutChecked = true
     var isSmoothEdges = false
     var isMaskOutChecked = false
-    var useCoinReference = true
+    var useCoinReference = false
 }
